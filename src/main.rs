abb8a4344c7de0c4320ab5be233c0690e6f294fd
LeHuman <47549331+LeHuman@users.noreply.github.com>
2024-06-21 13:25:27 -0500

    refactor: :art: Cleanup imports and comments

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 use std::env;
-use clap::{arg, command, Parser};
-use rand::Rng;
-use std::fs::OpenOptions;
+use std::fs;
 use std::io::Write;
 use std::path::Path;
-use std::{env, fs};
 
+use clap::{arg, command, Parser};
 use octocrab::Octocrab;
+use rand::Rng;
+
 use reposcrape::{
     cache::Cache,
     expand::ExpandedCache,
     query::{github::GHQuery, query::QueryInterface},
 };
-
 mod reposcrape;
 
 #[derive(Parser, Debug)]
@@ -24,23 +23,23 @@ struct Args {
 }
 
 fn html_test() {
-        // Parse the command-line arguments
-        let args = Args::parse();
+    // Parse the command-line arguments
+    let args = Args::parse();
 
-        // Define the directory and file paths
-        let out_dir = &args.output;
-        let index_path = &format!("{}/index.html", out_dir);
-    
-        // Use the GitHub token
-        let _github_token = env::var("GITHUB_TOKEN").expect("No GitHub token in env");
-    
-        // Create the directory if it doesn't exist
-        if !Path::new(out_dir).exists() {
-            fs::create_dir_all(out_dir).expect("Failed to create output directory");
-        }
-    
-        // Define the content for the index.html
-        let html_content = r#"
+    // Define the directory and file paths
+    let out_dir = &args.output;
+    let index_path = &format!("{}/index.html", out_dir);
+
+    // Use the GitHub token
+    let _github_token = env::var("GITHUB_TOKEN").expect("No GitHub token in env");
+
+    // Create the directory if it doesn't exist
+    if !Path::new(out_dir).exists() {
+        fs::create_dir_all(out_dir).expect("Failed to create output directory");
+    }
+
+    // Define the content for the index.html
+    let html_content = r#"
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -54,41 +53,41 @@ fn html_test() {
         </body>
         </html>
         "#;
-    
-        // Write the content to the index.html file
-        let mut file = fs::File::create(index_path).expect("Failed to create index.html file");
-        file.write_all(html_content.as_bytes())
-            .expect("Failed to write to index.html file");
-    
-        println!("Static site generated successfully at {}", index_path);
-    
-        // Define the directory and file paths
-        let dir_path = "./.cache";
-        let file_path = &format!("{}/output.txt", dir_path);
-    
-        // Check if the directory exists, and create it if it doesn't
-        if !Path::new(dir_path).exists() {
-            fs::create_dir(dir_path).expect("Failed to create .cache directory");
-        }
-    
-        // Check if the file already exists
-        if Path::new(file_path).exists() {
-            println!("File already exists: {file_path}");
-            let mut file = OpenOptions::new()
-                .append(true)
-                .open(file_path)
-                .expect("Failed to open output.txt file");
-            let mut rng = rand::thread_rng();
-            let random_number = rng.gen::<u32>().to_string();
-            file.write_all(random_number.as_bytes())
-                .expect("Failed to write to file");
-        } else {
-            // Create and write to the file
-            let mut file = fs::File::create(file_path).expect("Failed to create output.txt file");
-            file.write_all(b"Hello, this is a sample text.")
-                .expect("Failed to write to file");
-            println!("File written to {file_path}");
-        }
+
+    // Write the content to the index.html file
+    let mut file = fs::File::create(index_path).expect("Failed to create index.html file");
+    file.write_all(html_content.as_bytes())
+        .expect("Failed to write to index.html file");
+
+    println!("Static site generated successfully at {}", index_path);
+
+    // Define the directory and file paths
+    let dir_path = "./.cache";
+    let file_path = &format!("{}/output.txt", dir_path);
+
+    // Check if the directory exists, and create it if it doesn't
+    if !Path::new(dir_path).exists() {
+        fs::create_dir(dir_path).expect("Failed to create .cache directory");
+    }
+
+    // Check if the file already exists
+    if Path::new(file_path).exists() {
+        println!("File already exists: {file_path}");
+        let mut file = fs::OpenOptions::new()
+            .append(true)
+            .open(file_path)
+            .expect("Failed to open output.txt file");
+        let mut rng = rand::thread_rng();
+        let random_number = rng.gen::<u32>().to_string();
+        file.write_all(random_number.as_bytes())
+            .expect("Failed to write to file");
+    } else {
+        // Create and write to the file
+        let mut file = fs::File::create(file_path).expect("Failed to create output.txt file");
+        file.write_all(b"Hello, this is a sample text.")
+            .expect("Failed to write to file");
+        println!("File written to {file_path}");
+    }
 }
 
 async fn example(cache_file: &str) -> Result<(), Box<dyn std::error::Error>> {
@@ -99,9 +98,6 @@ async fn example(cache_file: &str) -> Result<(), Box<dyn std::error::Error>> {
         .build()?;
     let query = GHQuery::new(octocrab);
 
-    // let mut fetched = query
-    // .fetch_after("LeHuman", 8, Date::from_date_str(&cache.last_update)?)
-    // .await?;
     let fetched = query.fetch_latest("LeHuman", 8).await?;
 
     cache.update(&fetched);
