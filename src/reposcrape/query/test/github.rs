c215d206554e64177429e83596ea310ddbb506a0
LeHuman <47549331+LeHuman@users.noreply.github.com>
2024-06-21 14:54:43 -0500

    test: :heavy_minus_sign: use GITHUB_TOKEN for query test

--- src/reposcrape/query/test/github.rs
+++ src/reposcrape/query/test/github.rs
@@ -1,6 +1,5 @@
-use dotenv::dotenv;
 use octocrab::Octocrab;
-use secrecy::SecretString;
+use std::env;
 
 use crate::reposcrape::{
     query::{github::GHQuery, query::QueryInterface},
@@ -8,18 +7,15 @@ use crate::reposcrape::{
 };
 
 pub async fn test_github_retrieve() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv()?;
-    let personal_token = SecretString::new(String::from(std::env::var("PERSONAL_GITHUB_TOKEN")?));
-    let octocrab = Octocrab::builder().personal_token(personal_token).build()?;
+    // let personal_token = SecretString::new(String::from(std::env::var("PERSONAL_GITHUB_TOKEN")?));
+    let octocrab = Octocrab::builder()
+        .user_access_token(env::var("GITHUB_TOKEN").expect("No Github token"))
+        .build()?;
     let query = GHQuery::new(octocrab);
 
     let _latest = query.fetch_latest("LeHuman", 8).await?;
     let _dated = query
-        .fetch_after(
-            "LeHuman",
-            4,
-            Epoch::from_rfc3339("2022-05-14T19:19:26Z")?,
-        )
+        .fetch_after("LeHuman", 4, Epoch::from_rfc3339("2022-05-14T19:19:26Z")?)
         .await?;
 
     Ok(())
