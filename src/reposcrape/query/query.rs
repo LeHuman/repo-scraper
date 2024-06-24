2ced5db54f8a07f3ec084a73356b8ecb1089bc82
LeHuman <47549331+LeHuman@users.noreply.github.com>
2024-06-24 00:05:35 -0500

    chore: Add TODO: reduce query using cache

--- src/reposcrape/query/query.rs
+++ src/reposcrape/query/query.rs
@@ -3,6 +3,7 @@ use std::collections::BTreeSet;
 
 pub type QueryResult = Result<BTreeSet<Repo>, Box<dyn std::error::Error>>;
 
+//TODO: pass cache to compare and reduce redundant readme data from being queried
 pub trait QueryInterface {
     async fn fetch_latest(&self, user: &str, max_count: u32) -> QueryResult;
     async fn fetch_after(&self, user: &str, max_count: u32, after_epoch: EpochType) -> QueryResult;
